0e45a362ca80be0c7646db068fff712acb4c5d5a
ttommyy <tunger10@gmail.com>
2015-06-11 14:23:27 -0400

    building hash

--- hash/hash_it.c
+++
@@ -1,7 +0,0 @@
-#include "uthash.h"
-
-struct my_struct {
-  int id;                    /* key */
-  char name[10];
-  UT_hash_handle hh;         /* makes this structure hashable */
-};
--- my_hash/my_struct_hash.c
+++ my_hash/my_struct_hash.c
@@ -4,32 +4,33 @@
 
 typedef struct {
   char a;
-  int b;
+  //  int b;
 } record_key_t;
 
 typedef struct {
   record_key_t key;
-  /* ... other data ... */
+  int id;
+/* ... other data ... */
   UT_hash_handle hh;
 } record_t;
 
 int main(int argc, char *argv[]) {
   record_t l, *p, *r, *tmp, *records = NULL;
 
+  //build the record itself
   r = (record_t*)malloc( sizeof(record_t) );
   memset(r, 0, sizeof(record_t));
   r->key.a = 'a';
-  r->key.b = 1;
+  //  r->key.b = 1;
   HASH_ADD(hh, records, key, sizeof(record_key_t), r);
-
+  
+  //build a matching key to use to find the record
   memset(&l, 0, sizeof(record_t));
-  l.key.a = 'b';
-  l.key.b = 1;
-
+  l.key.a = 'a';
+  //  l.key.b = 1;
   HASH_FIND(hh, records, &l.key, sizeof(record_key_t), p);
 
-  if (p) printf("found %c %d\n", p->key.a, p->key.b);
-  if (r) printf("found %c %d\n", r->key.a, r->key.b);
+  if (p) printf("found %c %d \n", p->key.a, p->id);
 
   HASH_ITER(hh, records, p, tmp) {
     HASH_DEL(records, p);
